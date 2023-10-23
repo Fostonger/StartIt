266ecdf949d45e82d5cc50cb6d005c831b77a769
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2023-10-23 23:25:30 +0300

    Initial Commit

---
+++ StartIt/StartItApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartItApp.swift
+//  StartIt
+//
+//  Created by Булат Мусин on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StartItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
